2ceb50e42825e4a4efdeb1ebe2394ece9fac44ba
Mra Bologhine <mra.bologhine@outlook.fr>
2020-05-08 16:37:13 +0200

    Changed Test values

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching1"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
